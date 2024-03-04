bcab061cf16306f56bef6e0bc31b99f756dacf6e
Manohar K <manohar.k@sliceit.com>
2024-03-04 15:12:03 +0530

    Super duper code

--- src/main/java/RandomFile.java
+++ src/main/java/RandomFile.java
@@ -27,4 +27,16 @@ public class RandomFile {
             System.out.println("Error" + e.getMessage());
         }
     }
+
+    public void SubtractTheNumbers(int a, int b){
+        try{
+            if(a > 0 && b > 0){
+                System.out.println(a - b);
+            } else{
+                throw new Exception("Invalid input");
+            }
+        }catch(Exception e){
+            System.out.println("Error" + e.getMessage());
+        }
+    }
 }
