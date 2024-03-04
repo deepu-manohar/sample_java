8573c05365ad369900c602927c9924dd75cc1ac6
Manohar K <manohar.k@sliceit.com>
2024-03-04 15:22:00 +0530

    Super Duper Code

---
+++ src/test/java/RandomFileTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class RandomFileTest {
+    @Test
+    public void MultiplyTheNumbersTest() {
+        RandomFile randomFile = new RandomFile();
+        randomFile.MultiplyTheNumbers(2, 3);
+    }
+}
